d9811e6c5de5b28124276b12717d84124f7dd5be
neverovsky <neverovsky@gmail.com>
2021-04-30 11:27:26 +0300

    add mode = zpl for filter messages

--- content.js
+++ content.js
@@ -1,10 +1,16 @@
-console.log('Load content.js');
+const DEVELOPER_MODE  = !('update_url' in chrome.runtime.getManifest());;
+
+if(DEVELOPER_MODE){
+    console.log('Load content.js',chrome.runtime.getManifest());
+}
 
 window.addEventListener("message", receiveMessage, false);
 
 
 function receiveMessage(event) {
-    chrome.runtime.sendMessage(event.data, function (response) {
-        console.log(response);
-    });
+    if (event.data && event.data.type && event.data.type == 'zpl') {
+        chrome.runtime.sendMessage(event.data, function (response) {
+            console.log(response);
+        });
+    }
 }
